c124077368dc22696759378b71b5e489d02d5407
Dan Sover <dan.sover@avalabs.org>
2023-08-01 14:22:36 -0400

    fix: simplify client
    Signed-off-by: Dan Sover <dan.sover@avalabs.org>

--- examples/client.rs
+++ examples/client.rs
@@ -1,69 +1,63 @@
 use std::{
     env::args,
-    io::{self, Error, ErrorKind},
-    net::IpAddr,
+    io::{self, Write},
+    net::TcpStream,
     sync::Arc,
 };
 
-use hyper_rustls::ConfigBuilderExt;
-use rcgen::CertificateParams;
-use rustls::{ClientConfig, ClientConnection, ServerName};
+use rustls::{OwnedTrustAnchor, RootCertStore, ServerName};
 use tracing::info;
 
 /// cargo run --example client -- [PEER IP] [STAKING PORT]
-/// cargo run --example client -- 127.0.0.1 9649
+/// cargo run --example client -- 8.8.8.8 443 (works)
+/// cargo run --example client -- www.rust-lang.org 443 (works)
+/// cargo run --example client -- 52.47.181.114 9651 (returns server cert but no peer cert)
 fn main() -> io::Result<()> {
     let _ = env_logger::builder()
         .filter_level(log::LevelFilter::Trace)
         .try_init();
 
     // get args
-    let peer_ip = args().nth(1).expect("no peer IP given");
-    let peer_ip: IpAddr = peer_ip.parse().unwrap();
 
+    let server = args().nth(1).expect("no server given");
     let port = args().nth(2).expect("no port given");
-    let _port: u16 = port.parse().unwrap();
+    let sock_addr = format!("{}:{}", server, port);
 
-    let client_key_path = random_manager::tmp_path(10, None)?;
-    let client_cert_path = random_manager::tmp_path(10, None)?;
-    let client_cert_sna_params = CertificateParams::new(vec!["127.0.0.1".to_string()]);
-    cert_manager::x509::generate_and_write_pem(
-        Some(client_cert_sna_params),
-        &client_key_path,
-        &client_cert_path,
-    )?;
-    log::info!("client cert path: {}", client_cert_path);
-
-    let (private_key, certificate) = cert_manager::x509::load_pem_key_cert_to_der(
-        client_key_path.as_ref(),
-        client_cert_path.as_ref(),
-    )?;
-
-    // ref. https://docs.rs/rustls/latest/rustls/struct.ConfigBuilder.html#method.with_client_auth_cert
-    let client_config = ClientConfig::builder()
+    let mut root_store = RootCertStore::empty();
+    root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+        OwnedTrustAnchor::from_subject_spki_name_constraints(
+            ta.subject,
+            ta.spki,
+            ta.name_constraints,
+        )
+    }));
+    let config = rustls::ClientConfig::builder()
         .with_safe_defaults()
-        .with_native_roots()
-        .with_client_auth_cert(vec![certificate], private_key)
-        .map_err(|e| {
-            Error::new(
-                ErrorKind::Other,
-                format!("failed to create TLS client config '{}'", e),
-            )
-        })?;
-
-    let server_name = ServerName::try_from(peer_ip.to_string().as_str())
-        .map_err(|e| Error::new(ErrorKind::InvalidInput, format!("{e}")))?;
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
 
     // this creates the connection (or session)
     // ref. go/crypto/tls/Conn#clientHandshake
     // Does the TLS handshake
-    let client = ClientConnection::new(Arc::new(client_config), server_name)
-        .map_err(|e| Error::new(ErrorKind::Other, format!("client connection '{}'", e)))?;
+    let server_name: ServerName = ServerName::try_from(server.as_ref()).unwrap();
+    let mut conn = rustls::ClientConnection::new(Arc::new(config), server_name).unwrap();
+    let mut sock = TcpStream::connect(sock_addr).unwrap();
+    let mut tls = rustls::Stream::new(&mut conn, &mut sock);
+    let _ = tls.write_all(
+        concat!(
+            "GET / HTTP/1.1\r\n",
+            "Host: www.rust-lang.org\r\n",
+            "Connection: close\r\n",
+            "Accept-Encoding: identity\r\n",
+            "\r\n"
+        )
+        .as_bytes(),
+    );
 
     info!("retrieving peer certificates...");
-    match client.peer_certificates() {
+    match conn.peer_certificates() {
         Some(peer_certs) => {
-            println!("peer certs: {:?}", peer_certs);
+            println!("\n\n PEER CERTS:\n {:#?}\n\n", peer_certs);
         }
         None => {
             println!("no peer certs");
